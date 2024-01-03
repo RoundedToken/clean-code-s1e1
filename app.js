48bf84feef19e0aee7737fda83a41c1ff075b887
RoundedToken <117864556+RoundedToken@users.noreply.github.com>
2024-01-03 18:08:41 +0100

    fix(extended-2.1, basic-3.1, basic-3.2, basic-3.3, basic-3.4): migrated all selectors to BEM and removed tag selectors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -33,20 +33,22 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task_not-editable';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className='tasks-list__item__checkbox'
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task_editable";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button_edit button_default";
 
-    deleteButton.className="delete";
+    deleteButton.className="button_delete button_default";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="button__image_delete"
     deleteButton.appendChild(deleteButtonImg);
-
+    listItem.classList.add("tasks-list__item")
 
     //and appending.
     listItem.appendChild(checkBox);
@@ -84,7 +86,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".button_edit");
     var containsClass=listItem.classList.contains("editMode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -156,8 +158,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.button_edit");
+    var deleteButton=taskListItem.querySelector("button.button_delete");
 
 
     //Bind editTask to edit button.
